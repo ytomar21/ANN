8d38ba08e1e0ef35bd9f35ffeae791978b2055d9
Yajur Tomar <yajurtomar@Yajurs-MacBook-Pro.local>
2025-06-02 11:30:25 -0400

    Updated image name

--- ann.py
+++ ann.py
@@ -2,14 +2,16 @@ import torch.nn as nn
 
 
 class ANN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size, dropout=0.2):
         super(ANN, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
+        self.dropout = nn.Dropout(p=dropout)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         x = self.fc1(x)
+        x = self.dropout(x)
         x = self.relu(x)
         x = self.fc2(x)
         return x
